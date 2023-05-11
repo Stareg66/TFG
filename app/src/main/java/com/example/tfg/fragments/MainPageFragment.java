e71c90eade5e758a4c384a51c02cea6291a2e3e3
Manuel Rueda <manuruedaalgar@gmail.com>
2023-05-11 13:14:06 +0200

    Primera versión de página principal

--- app/src/main/java/com/example/tfg/fragments/MainPageFragment.java
+++ app/src/main/java/com/example/tfg/fragments/MainPageFragment.java
@@ -61,6 +61,8 @@ public class MainPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_mainpage, container, false);
     }
 }
